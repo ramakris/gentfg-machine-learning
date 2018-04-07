40bd8ce00e7076d6de96c7d0d0485fbb9c8ddbdb
bhupatra <ramakrishna.bhupathi@hpe.com>
2018-04-07 15:09:15 -0700

    added

--- week8/eigen.py
+++ week8/eigen.py
@@ -1,8 +1,12 @@
 from numpy import linalg as LA
 import numpy
-#x=numpy.array([[1,2,3] ,[4,5,7], [6,9,7]])
-x=numpy.array([[1,-2,] , [2,-3]])
+x=numpy.array([[1,2,3] ,[4,5,7], [6,9,7]])
+#x=numpy.array([[1,-2,] , [2,-3]])
 w,v=LA.eig(x)
 print w
 print v
+print "DOT Product x and v"
+print numpy.dot(x,v)
 
+print "multiplying w and v"
+print numpy.multiply(w,v)
